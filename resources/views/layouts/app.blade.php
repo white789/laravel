a3cd1e3a0fa64955143d29bec3d9e142adad4354
white789 <intensediscipline@gmail.com>
2022-07-08 14:56:20 +0100

    Installed laravel ui 3.0.0
    Added css and js to layout file

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -5,6 +5,8 @@
     <meta name="viewport"
           content="width=device-width, user-scalable=no, initial-scale=1.0, maximum-scale=1.0, minimum-scale=1.0">
     <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <link rel="stylesheet" href="{{ mix('css/app.css') }}">
+    <script src="{{ mix('js/app.js') }}" defer></script>
     <title>Laravel App - @yield('title')</title>
 </head>
 <body>
