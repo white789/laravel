3094887b29dc4cf5e27203b81017677df89bd2bf
white789 <intensediscipline@gmail.com>
2022-07-08 10:25:56 +0100

    Added posts resource controller Added methods to add, edit, delete posts

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -9,6 +9,11 @@
 </head>
 <body>
     <div>
+        @if(session('status'))
+            <div style="background-color: red; color: white">
+                {{ session('status') }}
+            </div>
+        @endif
         @yield('content')
     </div>
 </body>
--- resources/views/posts/partials/post.blade.php
+++ resources/views/posts/partials/post.blade.php
@@ -1,7 +1,15 @@
 {{--        @break($key == 2)--}}
 {{--    @continue($key == 1)--}}
 @if($loop->even)
-    <h1>{{ $key }}.{{ $post['title'] }}</h1>
+    <h1>{{ $key }}.{{ $post->title }}</h1>
 @else
-    <h1 style="background-color: silver">{{ $key }}.{{ $post['title'] }}</h1>
+    <h1 style="background-color: silver">{{ $key }}.{{ $post->title }}</h1>
 @endif
+
+<div>
+    <form action="{{ route('posts.destroy', ['post' => $post->id]) }}" method="POST">
+        @csrf
+        @method('DELETE')
+        <input type="submit" value="Delete">
+    </form>
+</div>
