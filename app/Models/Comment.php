7627ce65c788fccdc050ca306ac51c354050fe60
white789 <intensediscipline@gmail.com>
2022-07-15 15:52:22 +0100

    Setup relationships and tables

---
+++ app/Models/Comment.php
@@ -0,0 +1,18 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\Relations\BelongsTo;
+
+class Comment extends Model
+{
+    use HasFactory;
+
+    // blog_post_id
+    public function blogPost(): BelongsTo
+    {
+        return $this->belongsTo('App\Models\BlogPost');
+    }
+}
